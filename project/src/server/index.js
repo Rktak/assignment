6ada2156453ab524cb354d6b6565da7822e4716c
Rktak <2405rounaktak@gmail.com>
2021-06-30 14:21:26 +0530

    backend

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,40 +1,35 @@
-// Import Statements
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const { response } = require('express');
 
-// Configuration statements
 const port = 3001;
+
 app.use(bodyParser.json());
 
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Role');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+  })  
 
-// app.use((req,res,next) => {
-//   res.setHeader('Access-Control-Allow-Origin', "*");
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Role');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-//   next();
-// })
-// a
-// // Routing
-// app.use('/api/v1/user', );
-// app.use('/api/v1/admin', adminRoutes);
-// app.use('https://jsonplaceholder.typicode.com/posts');
+app.get('/postsId',(req,res) => {
+    axios(`https://jsonplaceholder.typicode.com/posts/${req.query.id}/comments`)
+    .then((response) => {res.send(JSON.stringify(response.data));} )
+    .catch((err)=>{console.log(err,"Error occured in posts comments")} );
+});
 
-
-// // Error Handling
-// app.use((req,res,next) => {
-//   const error = new HttpError('Page not found',404);
-//   throw error;
-// });
-
-// app.use((error, req, res, next) => {
-//   res.status(error.code);
-//   res.json({message: error.message || 'Unknown error occured' , code: error.code });
-// });
+app.get('/posts',(req,res) => {
+    let  respond ;
+    axios('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {respond = response.data; 
+        res.send(JSON.stringify(respond));} )
+    .catch((err)=>{console.log(err,"Error occured in posts")} );
+});
 
 
-app.get('/',funtion(req,res){
-
-})
-
-app.listen(3000,()=>console.log("running server at 3000"));
+app.listen(port,() => (
+    console.log(`Server is running on ${port}`)
+));
